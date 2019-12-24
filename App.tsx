a8f0e26a17e1a52396451a58c38ac3ec7da5a837
Ariel <ariels262626@gmail.com>
2019-12-24 16:10:08 +0200

    git conf

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Upstream Telematics app.</Text>
+      <Text>Welcome to Upstream Telematics app!</Text>
     </View>
   );
 }
